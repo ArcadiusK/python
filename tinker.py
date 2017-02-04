8d1e5ebcdf56b1db18a0067afac2f5a492124cce
Arcadius Kazimierski Javascript, Node.js, MongoDB, Angular.js, Express.js, C# .Net, SQL Server, Java, Android, Python <arcadiusk@gmail.com>
2017-02-04 01:26:18 -0500

    Update tinker.py

--- tinker.py
+++ tinker.py
@@ -5,7 +5,13 @@ print(pow(a,b))
 print(pow(a,b,m))
 a, b, c, d = int(input()), int(input()), int(input()), int(input())
 print(a**b + c**d)
+
 from itertools import product
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 print(*list(product(A, B)))
+
+from itertools import permutations
+word, p = map(str, input().split())
+for item in sorted(permutations(word, int(p))):
+     print("".join(item))
