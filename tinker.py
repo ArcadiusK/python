ab1728eea8ea50a5aa7e961d5ec89c7efdc96b8a
Arcadius Kazimierski Javascript, Node.js, MongoDB, Angular.js, Express.js, C# .Net, SQL Server, Java, Android, Python <arcadiusk@gmail.com>
2017-02-06 16:52:38 -0500

    Update tinker.py

--- tinker.py
+++ tinker.py
@@ -28,3 +28,8 @@ to_newyork_zone = tz.gettz('America/New_York')
 utc_time = datetime.datetime.strptime(datetime.now(), '%Y-%m-%dT%H:%M:%S.%fZ')
 utc_time = utc_time.replace(tzinfo=from_zone)
 eastern_time = utc_time.astimezone(to_newyork_zone)
+
+from itertools import combinations_with_replacement
+word, k = map(str, input().split())
+for item in list(combinations_with_replacement(sorted(word), int(k))):
+    print("".join(item))
