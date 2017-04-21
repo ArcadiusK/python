9a2d24f1d7df57294abe6b889a1658591f445e92
Arcadius Kazimierski Javascript, Python, C# .Net, SQL Server, Node.js, MongoDB, Angular.js, Express.js, Java, Android <arcadiusk@gmail.com>
2017-04-21 13:41:38 -0400

    Add millisecods format

--- tinker.py
+++ tinker.py
@@ -23,7 +23,7 @@ except:
     optimizer = tf.train.AdadeltaOptimizer().minimize(cost)
     raise
      
-print(time.strftime("%H:%M:%S", time.localtime()))
+print(time.strftime("%H:%M:%S.%f", time.localtime()))
      
 from collections import deque
 for i in range(int(input())):
