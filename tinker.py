f769544a389eab1fee3ff7d95b6dabd311823904
Arcadius Kazimierski Quantitative Software Engineer Tensorflow, Python, Javascript, C# .Net, SQL Server Database, Node.js, MongoDB, Angular.js, Express.js, Java, Android <arcadiusk@gmail.com>
2017-05-25 18:03:53 -0400

    Add parser for HTML

--- tinker.py
+++ tinker.py
@@ -30,6 +30,24 @@ else:
     print((-1, -1))
 
 
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):        
+        print ('Start :',tag)
+        for ele in attrs:
+            print ('->',ele[0],'>',ele[1])
+            
+    def handle_endtag(self, tag):
+        print ('End   :',tag)
+        
+    def handle_startendtag(self, tag, attrs):
+        print ('Empty :',tag)
+        for ele in attrs:
+            print ('->',ele[0],'>',ele[1])
+            
+MyParser = MyHTMLParser()
+MyParser.feed(''.join([input().strip() for _ in range(int(input()))]))
+
 s = re.search(r'([a-zA-Z0-9])\1', input())
 print(s.group(1) if s else -1)
 
