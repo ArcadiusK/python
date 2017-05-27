2f4dbdfc2102b6b47fc41f1b06900cd38164b5cc
Arcadius Kazimierski Quantitative Software Engineer Tensorflow, Python, Javascript, C# .Net, SQL Server Database, Node.js, MongoDB, Angular.js, Express.js, Java, Android <arcadiusk@gmail.com>
2017-05-27 13:50:35 -0400

    Add import regex

--- tinker.py
+++ tinker.py
@@ -49,6 +49,8 @@ class MyHTMLParser(HTMLParser):
 MyParser = MyHTMLParser()
 MyParser.feed(''.join([input().strip() for _ in range(int(input()))]))
 
+
+import re
 s = re.search(r'([a-zA-Z0-9])\1', input())
 print(s.group(1) if s else -1)
 
